539059edb65ece13cfc9e5fd65a7a0ccf56e1e73
sunny <jelpis27@gmail.com>
2023-02-22 19:06:11 +0900

    footer 1:36:40

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,8 +1,140 @@
 import React from 'react';
 import './footer.css';
+import video2 from '../../Assets/video2.mp4';
+import { FiSend } from 'react-icons/fi';
+import { MdTravelExplore } from 'react-icons/md';
+import { AiOutlineTwitter } from 'react-icons/ai';
+import { AiFillYoutube } from 'react-icons/ai';
+import { AiFillInstagram } from 'react-icons/ai';
+import { FaTripadvisor } from 'react-icons/fa';
+import { FiChevronRight } from 'react-icons/fi';
 
 const Footer = () => {
-  return <div>foorer</div>;
+  return (
+    <section className="footer">
+      <div className="videoDiv">
+        <video src={video2} loop autoPlay muted type="video/mp4"></video>
+      </div>
+
+      <div className="secContent container">
+        <div className="contactDiv flex">
+          <div className="text">
+            <small>KEEP IN TOUCH</small>
+            <h2>Travel with us</h2>
+          </div>
+
+          <div className="inputDiv flex">
+            <input type="text" placeholder="Enter Email Address" />
+            <button className="btn flex" type="submit">
+              SEND <FiSend className="icon" />
+            </button>
+          </div>
+        </div>
+
+        <div className="footerCard flex">
+          <div className="footerIntro flex">
+            <div className="logoDiv">
+              <a href="#" className="logo flex">
+                <MdTravelExplore className="icon" />
+                Travel.
+              </a>
+            </div>
+            <div className="footerParagraph">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque
+              officiis, sequi tempore, eligendi sunt alias saepe amet assumenda
+              vero aspernatur ad qui. Possimus molestiae praesentium modi vitae
+              totam voluptate laboriosam?
+            </div>
+            .
+            <div className="footerSocials flex">
+              <AiOutlineTwitter className="icon" />
+              <AiFillYoutube className="icon" />
+              <AiFillInstagram className="icon" />
+              <FaTripadvisor className="icon" />
+            </div>
+          </div>
+
+          <div className="footerLinks grid">
+            {/* Group One */}
+            <div className="linkGroup">
+              <span className="groupTitle">OUR AGENCY</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Services
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Insurance
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Agency
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Tourism
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Payment
+              </li>
+            </div>
+            {/* Group Two */}
+            <div className="linkGroup">
+              <span className="groupTitle">PARTNERS</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Bookings
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Rentcars
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                HotelWorld
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Trivago
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                TripAdvisor
+              </li>
+            </div>
+            {/* Group Three */}
+            <div className="linkGroup">
+              <span className="groupTitle">LAST MINUTE</span>
+
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                London
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                California
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Indonesia
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Europe
+              </li>
+              <li className="footerList flex">
+                <FiChevronRight className="icon" />
+                Oceania
+              </li>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
