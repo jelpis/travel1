e7489ea32fe43234f7df89984f4728ef912ccba1
sunny <jelpis27@gmail.com>
2023-02-22 11:29:45 +0900

    main ing

--- src/Components/Main/main.jsx
+++ src/Components/Main/main.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './main.css';
+import img from '../../Assets/img1.jpg';
+import { HiOutlineLocationMarker } from 'react-icons/hi';
+import { HiOutlineClipboardCheck } from 'react-icons/hi';
 
 const Data = [
   {
@@ -104,7 +107,47 @@ const Main = () => {
       <h3 className="title">Most visited destinatins</h3>
     </div>
 
-    <div className="secContent grid"></div>
+    <div className="secContent grid">
+      {Data.map(
+        ({ id, imgSrc, destTitle, location, grade, fees, description }) => {
+          return (
+            <div key={id} className="singleDestination">
+              <div className="imageDiv">
+                <img src={imgSrc} alt={destTitle} />
+              </div>
+
+              <div className="cardInfo">
+                <h4 className="destTitle"></h4>
+                <span className="continent flex">
+                  <HiOutlineLocationMarker className="icon" />
+                  <span className="name">{location}</span>
+                </span>
+
+                <div className="fees flex">
+                  <div className="grade">
+                    <span>
+                      {grade}
+                      <small>+1</small>
+                    </span>
+                  </div>
+                  <div className="price">
+                    <h5>{fees}</h5>
+                  </div>
+                </div>
+
+                <div className="desc">
+                  <p>{description}</p>
+                </div>
+
+                <button className="btn flex">
+                  DETAILS <HiOutlineClipboardCheck className="icon" />
+                </button>
+              </div>
+            </div>
+          );
+        }
+      )}
+    </div>
   </section>;
 };
 
